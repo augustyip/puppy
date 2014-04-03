3cc3b06b18021fbc6331bc22a0171c2b2ab6b752
August Yip <augustyip@gmail.com>
2014-04-03 11:01:33 +0800

    enhance the counter.
    Signed-off-by: August Yip <augustyip@gmail.com>

--- puppy.py
+++ puppy.py
@@ -2,12 +2,11 @@ import urllib.request
 import sys
 import socket
 
-success_res = ['200', '500', '301', '302', '403']
-web_apps = ['php', 'asp', 'jsp']
 
 def find_admin_login(url) :
-  global success_res
-  global web_apps
+
+  success_res = ['200', '500', '301', '302', '403']
+  web_apps = ['php', 'asp', 'jsp']
 
   with open('dict/admin_short.txt', 'r') as file_content:
     admin_context_paths = file_content.read().split('\n')
@@ -31,25 +30,24 @@ def find_admin_login(url) :
 
   total_urls = len(request_urls)
   i = 1
-
+  counter = ''
   for request_url in request_urls :
-    counter = 'Searching: ' + str(i) + '/' + str(total_urls)
+    sys.stdout.write("\b" * len(counter))
+    counter = 'Searching: (' + str(i) + '/' + str(total_urls) + ')'
     try:
       request = urllib.request.Request(request_url)
       response = urllib.request.urlopen(request, timeout = 3)
       if str(response.status) in success_res :
-        sys.stdout.write("\b" * len(counter))
         print(str(response.status) + ' ' + request_url)
     except urllib.error.HTTPError as e:
       if str(e.code) in success_res :
-        sys.stdout.write("\b" * len(counter))
         print(str(e.code) + ' ' + request_url)
     except socket.timeout as e:
       e
-    print(counter, end='\r')
+    print(counter, end = '\r')
     i += 1
 
   return response.status
 
-url = 'http://delbert.me/'
+url = 'http://google.com/'
 find_admin_login(url);
