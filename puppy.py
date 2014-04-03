f98834d9245508e70443365d8d40432dee8ac5bd
August Yip <augustyip@gmail.com>
2014-04-03 10:38:32 +0800

    finish find_admin_login function .
    Signed-off-by: August Yip <augustyip@gmail.com>

--- puppy.py
+++ puppy.py
@@ -1,45 +1,55 @@
 import urllib.request
-import re
+import sys
+import socket
 
 success_res = ['200', '500', '301', '302', '403']
 web_apps = ['php', 'asp', 'jsp']
 
-def main(url) :
+def find_admin_login(url) :
   global success_res
   global web_apps
 
-  with open('data/admins.txt', 'r') as file_content:
+  with open('dict/admin_short.txt', 'r') as file_content:
     admin_context_paths = file_content.read().split('\n')
 
+  request_urls = []
+
+
   for path in admin_context_paths:
-    # path = re.sub('/\s\s+/', ' ', path)
-    path.rstrip()
-    # pattern = re.compile(r'\s+')
-    # path = re.sub(pattern, '', path)
-    if '%EXT%' in path :
+    path.strip()
+    if len(path) == 0 :
+      continue
+
+    if '{ext}' in path :
       for web_app in web_apps:
-        replaced_path = path.replace('%EXT%', web_app)
+        replaced_path = path.replace('{ext}', web_app)
         request_url = url + replaced_path if url.endswith('/') else url + '/' + replaced_path
-        try:
-          request = urllib.request.Request(request_url)
-          response = urllib.request.urlopen(request, timeout = 3)
-          if str(response.status) in success_res :
-            print (str(response.status) + ' ' + request_url)
-        except urllib.error.HTTPError as e:
-          if str(e.code) in success_res :
-            print (str(e.code) + ' ' + request_url)
+        request_urls.append(request_url)
     else :
       request_url = url + path if url.endswith('/') else url + '/' + path
-      try:
-        request = urllib.request.Request(request_url)
-        response = urllib.request.urlopen(request, timeout = 3)
-        if str(response.status) in success_res :
-          print (str(response.status) + ' ' + request_url)
-      except urllib.error.HTTPError as e:
-        if str(e.code) in success_res :
-          print (str(e.code) + ' ' + request_url)
+      request_urls.append(request_url)
+
+  total_urls = len(request_urls)
+  i = 1
+
+  for request_url in request_urls :
+    counter = 'Searching: ' + str(i) + '/' + str(total_urls)
+    try:
+      request = urllib.request.Request(request_url)
+      response = urllib.request.urlopen(request, timeout = 3)
+      if str(response.status) in success_res :
+        sys.stdout.write("\b" * len(counter))
+        print(str(response.status) + ' ' + request_url)
+    except urllib.error.HTTPError as e:
+      if str(e.code) in success_res :
+        sys.stdout.write("\b" * len(counter))
+        print(str(e.code) + ' ' + request_url)
+    except socket.timeout as e:
+      e
+    print(counter, end='\r')
+    i += 1
 
   return response.status
 
 url = 'http://delbert.me/'
-main(url);
+find_admin_login(url);
--- test.py
+++ test.py
@@ -1,17 +1,8 @@
+from progress.bar import Bar
 import time
-import sys
 
-toolbar_width = 40
-
-# setup toolbar
-sys.stdout.write("[%s]" % (" " * toolbar_width))
-sys.stdout.flush()
-sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
-
-for i in range(toolbar_width):
-    time.sleep(1) # do real work here
-    # update the bar
-    sys.stdout.write( str(i) + "/" + str(toolbar_width))
-    sys.stdout.flush()
-
-sys.stdout.write("\n")
+bar = Bar('Processing', max=20)
+for i in range(20):
+  bar.next()
+  time.sleep(1)
+bar.finish()
