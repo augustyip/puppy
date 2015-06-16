bba9b6c89d4ca8190255a0a1595df9324b446e4f
August Yip <augustyip@gmail.com>
2015-06-16 11:50:36 +0800

    use curses to handle the output

--- puppy.py
+++ puppy.py
@@ -2,63 +2,22 @@ import urllib.request
 import urllib.parse
 
 import json
+
 import sys
+import time
+
 import socket
 
+import curses
 
-def find_admin_login(url) :
-
-  success_res = ['200', '500', '301', '302']
-  web_apps = ['php', 'asp', 'jsp', 'aspx']
-
-  with open('dict/admin_long.txt', 'r') as file_content:
-    admin_context_paths = file_content.read().split('\n')
-
-  request_urls = []
-
-
-  for path in admin_context_paths:
-    path.strip()
-    if len(path) == 0 :
-      continue
-
-    if '{ext}' in path :
-      for web_app in web_apps:
-        replaced_path = path.replace('{ext}', web_app)
-        request_url = url + replaced_path if url.endswith('/') else url + '/' + replaced_path
-        request_urls.append(request_url)
-    else :
-      request_url = url + path if url.endswith('/') else url + '/' + path
-      request_urls.append(request_url)
-
-  total_urls = len(request_urls)
-  i = 1
-  counter = ''
-  for request_url in request_urls :
-    sys.stdout.write("\b" * len(counter))
-    counter = 'Searching: (' + str(i) + '/' + str(total_urls) + ')'
-    i += 1
-    try:
-      request = urllib.request.Request(request_url)
-      response = urllib.request.urlopen(request, timeout = 3)
-      if str(response.status) in success_res :
-        print(str(response.status) + ' ' + request_url)
-    except urllib.error.HTTPError as e:
-      if str(e.code) in success_res :
-        print(str(e.code) + ' ' + request_url)
-    except socket.timeout as e:
-      continue
-    print(counter, end = '\r')
-
-  return response.status
-
+import traceback
 
 def quotes() :
 
   endpoint = 'https://query.yahooapis.com/v1/public/yql?'
 
   params = {
-    'q' : 'select * from yahoo.finance.quotes where symbol in ("0001.hk","AAPL","GOOG","MSFT")',
+    'q' : 'select * from yahoo.finance.quotes where symbol in ("0001.hk","0005.hk")',
     'format' : 'json',
     'env' : 'store://datatables.org/alltableswithkeys'
   }
@@ -71,14 +30,70 @@ def quotes() :
 
   query_result = json.loads(response.read().decode("utf-8"))
 
+
   if len(query_result['query']['results']['quote']) > 0 :
-    for quote in query_result['query']['results']['quote'] :
-      print('{0:25} {1} {2}'.format(quote['Name'], quote['LastTradePriceOnly'].rjust(10), quote['ChangeinPercent']))
-      # print(quote['Name'] + ' : ' + quote['LastTradePriceOnly'] + quote['ChangeinPercent'])
+    return query_result['query']['results']['quote']
 
-  # print(len(query_result['query']['results']['quote']))
 
+def main(stdscr) :
+  try:
+    stdscr.refresh()
+    # Your Code Stuff Here...
+    stdscr.addstr(1,1, "Press Any Key to Exit...")
+    # stdscr.getch()
+    i = 0
+    while True:
+      quote = {}
+      quote = quotes()
+
+      title = '{0:25} {1} {2}'.format('Name', 'LastTradePriceOnly', 'ChangeinPercent')
+
+      stdscr.addstr(3,1, title)
+
+      y = 3
+      for q in quote:
+        y = y + 1
+        data = '{0:25} {1} {2}'.format(q['Name'], q['LastTradePriceOnly'].rjust(10), q['ChangeinPercent'])
+        stdscr.addstr(y,1, data)
+      stdscr.refresh()
+      time.sleep(3)
+    
+  finally:
+    curses.endwin()
 
 
 if __name__ == '__main__':
-  quotes();
+  # quotes();
+  try :
+
+    # Initialize curses
+    stdscr=curses.initscr()
+
+    # Turn off echoing of keys, and enter cbreak mode,
+    # where no buffering is performed on keyboard input
+    curses.noecho()
+    curses.cbreak()
+    
+    # In keypad mode, escape sequences for special keys
+    # (like the cursor keys) will be interpreted and
+    # a special value like curses.KEY_LEFT will be returned
+    stdscr.keypad(1)
+    main(stdscr)
+
+    # Enter the main loop
+    # Set everything back to normal
+    stdscr.keypad(0)
+    curses.echo()
+    curses.nocbreak()
+    curses.endwin()
+    # Terminate curses
+
+  except :
+
+    # In event of error, restore terminal to sane state.
+    stdscr.keypad(0)
+    curses.echo()
+    curses.nocbreak()
+    curses.endwin()
+    traceback.print_exc()
+    # Print the exception
