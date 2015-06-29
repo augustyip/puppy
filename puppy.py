d9112f88f4ce32cc3bdcd3840a87e7c5458b703c
August Yip <augustyip@gmail.com>
2015-06-29 11:29:55 +0800

    implement exit method

--- puppy.py
+++ puppy.py
@@ -24,6 +24,9 @@ def main() :
 
       stdscr.addstr(0, 0, 'puppy - version: 0.9.3.14159265')
       stdscr.addstr(1, 0, 'current data source: ' + config.source + ', last refresh: ' + time.strftime('%H:%M:%S'))
+      stdscr.addstr(23, 0, 'Press 0 Key to Exit...')
+      
+
 
       if config.source == 'yahoo' :
         quotes = yahoo.quotes()
@@ -68,7 +71,14 @@ def main() :
         }
 
         stdscr.addstr(row, 0, placeholder_str.format(**data))
+
+      key = stdscr.getch()
+
+      if key == ord('0') :
+        sys.exit(0)
       stdscr.refresh()
+
+
       # time.sleep(float(config['Yahoo']['refresh']))
 
   finally :
