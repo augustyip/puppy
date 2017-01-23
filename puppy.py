ef636883777c9661c06a290218bf2af809f05987
August Yip <augustyip@gmail.com>
2017-01-23 12:37:00 +0800

    restruct main function

--- puppy.py
+++ puppy.py
@@ -17,13 +17,23 @@ from lib.source.yahoo import yahoo
 from lib.source.tencent import tencent
 
 def main() :
-
   try :
-    stdscr.addstr(1,1, 'Loading...')
-    stdscr.refresh()
+
+    # Initialize curses
+    stdscr = curses.initscr()
+
+    # Turn off echoing of keys, and enter cbreak mode,
+    # where no buffering is performed on keyboard input
+    curses.noecho()
+    curses.cbreak()
+
+    # In keypad mode, escape sequences for special keys
+    # (like the cursor keys) will be interpreted andh
+    # a special value like curses.KEY_LEFT will be returned
+    stdscr.keypad(1)
+
 
     while True:
-
       stdscr.addstr(0, 0, 'puppy - version: 0.9.3.14159265')
       stdscr.addstr(1, 0, 'current data source: ' + config.config['Default']['source'] + ', last refresh: ' + time.strftime('%H:%M:%S'))
       # stdscr.addstr(23, 0, 'Press 0 Key to Exit...')
@@ -74,29 +84,6 @@ def main() :
 
       stdscr.refresh()
 
-  finally :
-    curses.endwin()
-
-if __name__ == '__main__':
-
-  try :
-
-    # Initialize curses
-    stdscr=curses.initscr()
-
-    # Turn off echoing of keys, and enter cbreak mode,
-    # where no buffering is performed on keyboard input
-    curses.noecho()
-    curses.cbreak()
-
-    # In keypad mode, escape sequences for special keys
-    # (like the cursor keys) will be interpreted andh
-    # a special value like curses.KEY_LEFT will be returned
-    stdscr.keypad(1)
-    main()
-    # wrapper(main())
-
-    # Enter the main loop
     # Set everything back to normal
     stdscr.keypad(0)
     curses.echo()
@@ -105,11 +92,14 @@ if __name__ == '__main__':
     # Terminate curses
 
   except :
-
     # In event of error, restore terminal to sane state.
     stdscr.keypad(0)
     curses.echo()
     curses.nocbreak()
     curses.endwin()
     traceback.print_exc()
-    # Print the exception
+  finally :
+    curses.endwin()
+
+if __name__ == '__main__':
+  main()
